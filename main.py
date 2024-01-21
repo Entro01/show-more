cc32130212cf624cee581c418839fcbde20c14b0
Shubham Negi <shubham64negi@gmail.com>
2024-01-21 19:09:46 +0530

    made the first page and linked the second page

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def feature_extraction(img_path,model):
     return normalized_result
 
 def recommend(features,feature_list):
-    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+    neighbors = NearestNeighbors(n_neighbors=4, algorithm='brute', metric='euclidean')
     neighbors.fit(feature_list)
 
     distances, indices = neighbors.kneighbors([features])
@@ -46,21 +46,22 @@ def get_random_images():
 from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
+#app._static_folder = os.path.join(os.getcwd(), 'show-more/static')
 
 @app.route('/')
 def landing_page():
     random_images = get_random_images()
-    print(random_images)
+    #image_urls = [url_for('static', filename=img) for img in random_images]
     return render_template('landing.html', images=random_images)
 
-#@app.route('/recommend', methods=['POST'])
-#def recommend_page():
-#    user_image = request.files['image']
-#    user_image.save("temp.jpg")
-#    user_features = feature_extraction("temp.jpg", model)
-#    recommended_images = recommend(user_features, feature_list)
-#    image_urls = [url_for('static', filename=img) for img in recommended_images]
-#    return render_template('recommend.html', images=image_urls)
+@app.route('/recommend/<img_path>', methods=['GET'])
+def recommend_page(img_path):
+    img_path = 'static/'+img_path
+    user_features = feature_extraction(img_path, model)
+    recommended_indices = recommend(user_features, feature_list)
+    recommended_paths = [filenames[i] for i in recommended_indices[0]]
+    #image_urls = [url_for('static', filename=img) for img in recommended_images]
+    return render_template('recommend.html', images=recommended_paths)
 
 if __name__ == "__main__":
     app.run(debug=True)
